9713564682a0a2e647094691b18f635c91f6985f
friskaim7 <friska.intan2001@gmail.com>
2021-03-15 13:01:29 +0700

    finished

---
+++ W1/src/Another Type of Employee/Commission.java
@@ -0,0 +1,27 @@
+public class Commission extends Hourly {
+    // 12.47
+    double totalSales;
+    double commissionRate;
+
+    public Commission(String eName, String eAddress, String ePhone, String socSecNumber, double rate, double comRate) {
+        super(eName, eAddress, ePhone, socSecNumber, rate);
+        this.commissionRate = comRate;
+    }
+
+    public void addSales(double totalSales) {
+        this.totalSales += totalSales;
+    }
+
+    @Override
+    public double pay() {
+        double temp = this.totalSales;
+        this.totalSales = 0;
+        return super.pay() + (this.commissionRate * temp);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nTotal Sales: " + this.totalSales;
+    }//12.57
+
+}
