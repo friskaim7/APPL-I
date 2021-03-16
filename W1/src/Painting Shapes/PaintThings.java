e6d825c32f96934d14bd9f6dccf829446a78b149
friskaim7 <friska.intan2001@gmail.com>
2021-03-16 22:30:11 +0700

    finish

--- W1/src/Painting Shapes/PaintThings.java
+++ W1/src/Painting Shapes/PaintThings.java
@@ -17,13 +17,19 @@ public class PaintThings {
     public static void main(String[] args) {
         final double COVERAGE = 350;
         Paint paint = new Paint(COVERAGE);
-        Rectangle deck;
-        Sphere bigBall;
-        Cylinder tank;
-        double deckAmt, ballAmt, tankAmt;
+        
         // Instantiate the three shapes to paint
         // Compute the amount of paint needed for each shape
         // Print the amount of paint for each.
+        Rectangle deck = new Rectangle(20,35);;
+        Sphere bigBall = new Sphere(15);
+        Cylinder tank = new Cylinder(10, 30);
+        double deckAmt, ballAmt, tankAmt;
+        
+        deckAmt = paint.amount(deck);
+        ballAmt = paint.amount(bigBall);
+        tankAmt = paint.amount(tank);
+        
         DecimalFormat fmt = new DecimalFormat("0.#");
         System.out.println("\nNumber of gallons of paint needed...");
         System.out.println("Deck " + fmt.format(deckAmt));
