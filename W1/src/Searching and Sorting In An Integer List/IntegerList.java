23473de034d75f6eb8254b90f05debf439d7bbf4
friskaim7 <friska.intan2001@gmail.com>
2021-03-16 15:28:45 +0700

    finish step 3

--- W1/src/Searching and Sorting In An Integer List/IntegerList.java
+++ W1/src/Searching and Sorting In An Integer List/IntegerList.java
@@ -1,3 +1,4 @@
+
 // ****************************************************************
 // IntegerList.java
 //
@@ -7,64 +8,96 @@
 // ****************************************************************
 import java.util.Scanner;
 
-public class IntegerList
-{
-    int[] list; //values in the list
-    //-------------------------------------------------------
-    //create a list of the given size
-    //-------------------------------------------------------
-    public IntegerList(int size)
-    {
+public class IntegerList {
+    int[] list; // values in the list
+    // -------------------------------------------------------
+    // create a list of the given size
+    // -------------------------------------------------------
+
+    public IntegerList(int size) {
         list = new int[size];
     }
-    //-------------------------------------------------------
-    //fill array with integers between 1 and 100, inclusive
-    //-------------------------------------------------------
-    public void randomize()
-    {
-        for (int i=0; i<list.length; i++)
-            list[i] = (int)(Math.random() * 100) + 1;
+
+    // -------------------------------------------------------
+    // fill array with integers between 1 and 100, inclusive
+    // -------------------------------------------------------
+    public void randomize() {
+        for (int i = 0; i < list.length; i++)
+            list[i] = (int) (Math.random() * 100) + 1;
     }
-    //-------------------------------------------------------
-    //print array elements with indices
-    //-------------------------------------------------------
-    public void print()
-    {
-        for (int i=0; i<list.length; i++)
+
+    // -------------------------------------------------------
+    // print array elements with indices
+    // -------------------------------------------------------
+    public void print() {
+        for (int i = 0; i < list.length; i++)
             System.out.println(i + ":\t" + list[i]);
     }
-    //-------------------------------------------------------
-    //return the index of the first occurrence of target in the list.
-    //return -1 if target does not appear in the list
-    //-------------------------------------------------------
-    public int search(int target)
-    {
+
+    // -------------------------------------------------------
+    // return the index of the first occurrence of target in the list.
+    // return -1 if target does not appear in the list
+    // -------------------------------------------------------
+    public int search(int target) {
         int location = -1;
-        for (int i=0; i<list.length && location == -1; i++)
+        for (int i = 0; i < list.length && location == -1; i++)
             if (list[i] == target)
-        location = i;
+                location = i;
         return location;
     }
 
-    //-------------------------------------------------------
-    //sort the list into ascending order using the selection sort algorithm
-    //-------------------------------------------------------
-    public void selectionSort()
-    {
+    // -------------------------------------------------------
+    // sort the list into ascending order using the selection sort algorithm
+    // -------------------------------------------------------
+    public void selectionSort() {
         int minIndex;
-        for (int i=0; i < list.length-1; i++)
-        {
-            //find smallest element in list starting at location i
+        for (int i = 0; i < list.length - 1; i++) {
+            // find smallest element in list starting at location i
             minIndex = i;
-            for (int j = i+1; j < list.length; j++)
+            for (int j = i + 1; j < list.length; j++)
                 if (list[j] < list[minIndex])
-            minIndex = j;
+                    minIndex = j;
 
-            //swap list[i] with smallest element
+            // swap list[i] with smallest element
             int temp = list[i];
 
             list[i] = list[minIndex];
             list[minIndex] = temp;
         }
     }
+
+    public void replaceFirst(int oldVal, int newVal) {
+        int location = this.search(oldVal);
+        if (location > -1)
+            list[location] = newVal;
+    }
+
+    public void replaceAll(int oldVal, int newVal) {
+        for (int element : list) {
+            if (element == oldVal) {
+                element = newVal;
+            }
+        }
+    }
+
+    public void sortDecreasing()
+    {
+        int maxIndex;
+        for (int i = 0; i < list.length - 1; i++) {
+            // find smallest element in list starting at location i
+            maxIndex = i;
+            for (int j = i + 1; j < list.length; j++)
+                if (list[j] > list[maxIndex])
+                    maxIndex = j;
+
+            // swap list[i] with smallest element
+            int temp = list[i];
+
+            list[i] = list[maxIndex];
+            list[maxIndex] = temp;
+    }
+
+    // public int binarySearchD(int target) {
+
+    // }
 }
