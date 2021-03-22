74a6407f16c705d6bcedef7aa61039636a4e384d
friskaim7 <friska.intan2001@gmail.com>
2021-03-22 18:07:19 +0700

    throw IllegalArgumentException on >16 value

--- W2/src/Throwing Exceptions/MathUtils.java
+++ W2/src/Throwing Exceptions/MathUtils.java
@@ -11,10 +11,13 @@ public class MathUtils {
     public static int factorial(int n) {
         if (n < 0) {
             throw new IllegalArgumentException(n + " is a negative value.");
+        } else if (n > 16) {
+            throw new IllegalArgumentException(n + " is more than 16.");
+        } else {
+            int fac = 1;
+            for (int i = n; i > 0; i--)
+                fac *= i;
+            return fac;
         }
-        int fac = 1;
-        for (int i = n; i > 0; i--)
-            fac *= i;
-        return fac;
     }
 }
