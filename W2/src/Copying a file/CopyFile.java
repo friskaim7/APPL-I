1a0a49eb44d8bea1b508b81a1b364a9faab18c98
friskaim7 <friska.intan2001@gmail.com>
2021-03-23 06:20:16 +0700

    refactoring

--- W2/src/Copying a file/CopyFile.java
+++ W2/src/Copying a file/CopyFile.java
@@ -3,23 +3,21 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class CopyFile {
     public static void main(String[] args) {
-        String fileName = "";
-        Scanner fileNameScanner = new Scanner(System.in);
-        while (fileName.isEmpty()) {
+        String fileName;
+        do {
             System.out.print("Enter a file path : ");
+            Scanner fileNameScanner = new Scanner(System.in);
             fileName = fileNameScanner.nextLine();
             try {
                 File file = new File(fileName);
                 Scanner fileScanner = new Scanner(file);
                 while (fileScanner.hasNextLine())
                     System.out.println(fileScanner.nextLine());
-                fileScanner.close();
-                fileNameScanner.close();
                 break;
             } catch (FileNotFoundException e) {
                 System.out.println("File Not Found. Please try again.");
                 fileName = "";
             }
-        }
+        } while (fileName.isEmpty());
     }
 }
