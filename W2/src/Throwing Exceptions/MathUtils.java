fa82f0afc6f26de374ea1d4eee0d385f30207eda
friskaim7 <friska.intan2001@gmail.com>
2021-03-22 18:02:22 +0700

    throw IllegalArgumentException on negative values

--- W2/src/Throwing Exceptions/MathUtils.java
+++ W2/src/Throwing Exceptions/MathUtils.java
@@ -2,13 +2,16 @@
 // MathUtils.java
 //
 // Provides static mathematical utility functions.
-//
+// or throw an IllegalArgumentException.
 // ****************************************************************
 public class MathUtils {
     // -------------------------------------------------------------
     // Returns the factorial of the argument given
     // -------------------------------------------------------------
     public static int factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException(n + " is a negative value.");
+        }
         int fac = 1;
         for (int i = n; i > 0; i--)
             fac *= i;
