6485ea9106ad7415f703038719972944cad5aa5a
friskaim7 <friska.intan2001@gmail.com>
2021-03-25 17:11:02 +0700

    refactoring

--- W2/src/Exceptions Aren't Always Errors/CountLetters.java
+++ W2/src/Exceptions Aren't Always Errors/CountLetters.java
@@ -11,6 +11,7 @@ public class CountLetters {
     public static void main(String[] args) {
         int[] counts = new int[26];
         Scanner scan = new Scanner(System.in);
+        int i = 0;
 
         // get word from user
         System.out.print("Enter a single word (letters only, please): ");
@@ -22,15 +23,16 @@ public class CountLetters {
 
         // count frequency of each letter in string
         try {
-            for (int i = 0; i < word.length(); i++)
+            for (i = 0; i < word.length(); i++)
                 counts[word.charAt(i) - 'A']++;
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Not a letter " + e.toString());
+            // System.out.println("Not a letter " + e.toString());
+            System.out.println(word.charAt(i)+ " is not a letter ");
         }
 
         // print frequencies
         System.out.println();
-        for (int i = 0; i < counts.length; i++)
+        for (i = 0; i < counts.length; i++)
             if (counts[i] != 0)
                 System.out.println((char) (i + 'A') + ": " + counts[i]);
     }
